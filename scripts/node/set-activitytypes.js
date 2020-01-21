1b8fd0813ec080234879431648cbcd61fc3d3030
Michael Johansen <michael.johansen@nav.no>
2020-01-21 13:12:09 +0100

    Updated version number on package

---
+++ scripts/node/set-activitytypes.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const activityKoder = require("./AKTIVITETKODER");
+const TemaMapping = require("./TemaMapping");
+const TypeMapping = require("./TypeMapping");
+
+
+activityKoder.forEach(kode=>{
+  tabell.push()
+})
+console.log(activityKoder);
